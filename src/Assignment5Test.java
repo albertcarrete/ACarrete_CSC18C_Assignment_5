6a9d1881e99d9aa244addf6ecf499761d4e5455a
albertcarrete <albertcarrete@gmail.com>
2016-04-29 13:02:15 -0700

    Initial Push

---
+++ src/Assignment5Test.java
@@ -0,0 +1,21 @@
+
+public class Assignment5Test {
+
+	/* 0) Convert the stack class to a template stack class (the
+	 * existing class only works with integers) 
+	 * 
+	 * 1) Provide functionality that will increase the size of the 
+	 * stack if we try to push an item onto an already full stack
+	 * (resize the array)
+	 * 
+	 * 2) Demonstrate the functionality of the template stack class
+	 * with pushing/popping of integers, floats/doubles, strings,
+	 * and even simple classes if you wish to be very creative.
+	 * 
+	 * */
+	public static void main(String[] args) {
+
+
+	}
+
+}
